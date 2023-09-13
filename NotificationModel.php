908e458a441007dac39fc59be698f434646df486
Darwinraj18 <141041802+Darwinraj18@users.noreply.github.com>
2023-09-13 11:51:51 +0530

    NotificationModel.php

--- NotificationModel.php
+++ NotificationModel.php
@@ -4,7 +4,6 @@ require_once 'UtilityModel.php';
 class NotificationModel {
 
     // Get all Notifications
-    
     public function getNotifications() {
         $sql = 'select * from notification';
 
@@ -16,4 +15,33 @@ class NotificationModel {
         return $query->fetchAll(PDO::FETCH_ASSOC);
 
     }
+    //get notification by id
+   public function getNotificationById($id) {
+    $sql = "SELECT * FROM notification WHERE id = :id";
+    $dbc = UtilityModel::getDBConnection();
+    $query = $dbc->prepare($sql);
+    $query->bindParam(':id', $id);
+    $query->execute();
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
+    //delete notification
+    public function deleteNotificationById($id) {
+    $sql = "DELETE FROM notification WHERE id = :id";
+    $dbc = UtilityModel::getDBConnection();
+    $query = $dbc->prepare($sql);
+    $query->bindParam(':id', $id);
+    return $query->execute();
+}
+//create notification
+public function createNotification($id, $subcontent, $img, $des) {
+    $sql = "INSERT INTO `notification`(`id`, `subcontent`, `img`, `des`) VALUES (:id, :subcontent, :img,: )";
+    $dbc = UtilityModel::getDBConnection();
+    $query = $dbc->prepare($sql);
+    $query->bindParam(':id', $idR);
+    $query->bindParam(':subcontent', $subcontent);
+    $query->bindParam(':img', $img);
+    $query->bindParam(':des', $des);
+    return $query->execute();
+}
+
 }
