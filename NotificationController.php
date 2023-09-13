600c21605277cf9b5cb6a84c8f9f48dd751bfa3c
Darwinraj18 <141041802+Darwinraj18@users.noreply.github.com>
2023-09-13 11:56:08 +0530

    NotificationController.php

--- NotificationController.php
+++ NotificationController.php
@@ -6,8 +6,7 @@ class NotificationController extends BaseController
 {
 
     // Notification Reference
-
-    public function getNotificationAction()
+ public function getNotificationAction()
     {
         $model = new NotificationModel();
 
@@ -17,7 +16,48 @@ class NotificationController extends BaseController
 
     }
 
+
+    public function getNotificationByIdAction()
+    {
+        $id=$_GET['id'];// Get the notification ID from the request
+        $model = new NotificationModel();// Call the notificationmodel method to delete the notification by ID
+        $notification=$model->getNotificationById($id);
+        if($notification){
+            $this->sendOutput(json_encode($notification));
+        }else{
+           $this->sendOutput(json_encode(['error'=>'notification not found'])) ;
+        }
+
+    }
+public function deleteNotificationByIdAction() {
+  
+    $id = $_GET['id'];  // Get the notification ID from the request
     
+    
+    $model = new NotificationModel();// Call the notificationmodel method to delete the notification by ID
+    $success = $model->deleteNotificationById($id);
+    
+    // Check if the deletion was successful 
+    if ($success) {
+        // Send a success response 
+        $this->sendOutput(json_encode(['message' => 'Notification deleted']));
+    } else {
+        
+        $this->sendOutput(json_encode(['error' => 'Notification not found']), [self::APP_JSON, self::HTTP_NOT_FOUND]);
+    }
+}
+public function createNotification(){
+    $id = $_POST['id'];
+    $subcontent = $_POST['subcontent'];
+    $img = $_POST['img'];
+    $des= $_POST['des'];
+    $model=new NotificationModel();
+    $success=$model->createNotification($id,$subcontent,$img,$des);
+    if ($success) {
+        $this->sendOutput(json_encode(['message' => 'Notification created']));
+    } 
+
+}
 }
 
 ?>
