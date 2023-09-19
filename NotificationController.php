1b2616cb4fcb10586596078c73dbff3d94f9f5df
Darwinraj18 <141041802+Darwinraj18@users.noreply.github.com>
2023-09-19 13:02:42 +0530

    NotificationController.php

--- NotificationController.php
+++ NotificationController.php
@@ -29,10 +29,10 @@ class NotificationController extends BaseController
         }
 
     }
-public function deleteNotificationByIdAction() {
+
+    public function deleteNotificationByIdAction() {
   
     $id = $_GET['id'];  // Get the notification ID from the request
-    
     
     $model = new NotificationModel();// Call the notificationmodel method to delete the notification by ID
     $success = $model->deleteNotificationById($id);
@@ -44,21 +44,88 @@ public function deleteNotificationByIdAction() {
     } else {
         
         $this->sendOutput(json_encode(['error' => 'Notification not found']));
+    
     }
 }
- public function createNotification(){
-    $data = json_decode(file_get_contents("php://input"));//This PHP function reads the raw POST data from the request body
-    $id = $_POST['id'];
+public function updateNotificationByIdAction(){
+    $id=$_GET['id'];
     $subcontent = $_POST['subcontent'];
-    $img = $_POST['img'];
-    $des= $_POST['des'];
-    $model=new NotificationModel();
-    $success=$model->createNotification();
+    $img = $_FILES['img']['tmp_name'];
+    $des = $_POST['des'];
+    $link=$_POST['link'];
+    $model= new NotificationModel();
+    $success=$model->updateNotificationById($id,$subcontent,$img,$des,$link);
     if ($success) {
-        $this->sendOutput(json_encode(['message' => 'Notification created']));
-    } 
- 
-}
+        // Send a success response 
+        $this->sendOutput(json_encode(['message' => 'Notification deleted']));
+    } else {
+        
+        $this->sendOutput(json_encode(['error' => 'Notification not found']));
+    
+    }
 }
 
+public function createNotificationAction() {
+    $data = json_decode(file_get_contents("php://input"));
+    $subcontent = $_POST['subcontent'];
+    $img = $_FILES['img']['tmp_name'];
+    $des = $_POST['des'];
+    
+
+    $link = isset($_POST['link']) ? $_POST['link'] : '';    // Check if 'link' key exists and is not empty
+
+    $model = new NotificationModel();
+    $success = $model->createNotification($subcontent, $des, $img, $link);
+
+    if ($success) {
+        $this->sendOutput(json_encode(['message' => 'Notification created']));
+    } else {
+        $this->sendOutput(json_encode(['error' => 'Unable to create Notification']));
+    }
+}
+
+
+public function createCustomerAction(){
+    $data=json_decode(file_get_contents("php://input"));
+    $email=$_POST['email'];
+    $firstname=$_POST['firstname'];
+    $lastname=$_POST['lastname'];
+    $city=$_POST['city'];
+    $model=new NotificationModel();
+    $success=$model->createCustomer($email,$firstname,$lastname,$city);
+    if($success){
+        $this->sendOutput(json_encode(['message' => 'user created']));
+    } else {
+        $this->sendOutput(json_encode(['error' => 'Unable to create']));
+    }
+}
+//GET Customer
+public function getCustomerByIdAction()
+    {
+        $entity_id=$_GET['entity_id'];
+        $model = new NotificationModel();
+        $Customer=$model->getCustomerById($entity_id);
+        if($Customer){
+            $this->sendOutput(json_encode($Customer));
+        }else{
+           $this->sendOutput(json_encode(['error'=>'notification not found'])) ;
+        }
+
+    }
+    //DELETE Coustomer
+public function deleteCustomerByIdAction(){
+    $entity_id=$_GET['entity_id'];
+    $model= new NotificationModel();
+    $success=$model->deleteCustomerById($entity_id);
+    if ($success) {
+        // Send a success response 
+        $this->sendOutput(json_encode(['message' => 'Notification deleted']));
+    } else {
+        
+        $this->sendOutput(json_encode(['error' => 'Notification not found']));
+    
+    }
+}
+
+}
 ?>
